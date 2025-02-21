faf1184cc6878340cbe384d5e9b7448ed2248ed3
Jami40 <jnjami40@gmail.com>
2025-02-21 18:53:30 +0600

    User added

--- index.js
+++ index.js
@@ -4,9 +4,71 @@ const app=express()
 require('dotenv').config()
 const port=process.env.PORT || 5000
 
+// UHwbtNRttJigLGaJ
+// tasks_management
+
 
 app.use(express.json())
 app.use(cors())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.knxzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("TaskManagementSystem");
+    const userCollection = database.collection("user");
+
+    app.get('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await userCollection.findOne(query);
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        res.send(user);
+    
+});
+
+  app.post('/user/:email',async(req,res)=>{
+      const email=req.params.email
+      const query={email}
+      const user=req.body
+      const isExists=await userCollection.findOne(query)
+      if(isExists){
+          return res.send(isExists)
+      }
+
+      const result=await userCollection.insertOne({...user,timestamp:Date.now(),})
+      res.send(result)
+
+  })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/',(req,res)=>{
